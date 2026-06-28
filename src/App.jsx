406d4536df1bfc90f83dcc7bd3df9cb739d8f3b4
Gabbie-Darlin <gabdarlin@gmail.com>
2026-06-28 15:30:40 +0000

    Application created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+// Import the components and pages
+import Navbar from "./components/Navbar";
+
+// Import the pages
+import Home from "./pages/Home";
+import Tasks from "./pages/Tasks";
+import TaskDetails from "./pages/TaskDetails";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
+export default function App() {
 
   return (
     <>
-    
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/tasks" element={<Tasks />}></Route>
+        <Route path="/tasks/:id" element={<TaskDetails />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
     </>
   )
 }
 
-export default App
+
