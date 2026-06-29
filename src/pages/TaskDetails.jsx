4976c04612b08f3858f5c2e8c7a4bf5e18b537ef
Gabbie-Darlin <gabdarlin@gmail.com>
2026-06-29 16:39:24 +0000

    new pages

---
+++ src/pages/TaskDetails.jsx
@@ -0,0 +1,37 @@
+import { useParams } from "react-router-dom";
+
+const tasks = [
+    {
+        id: 1, 
+        title: "Study React Router" ,
+        desc: "Learn routing basics"
+    },
+    {
+        id: 2, 
+        title: "Build project",
+        desc: "Create SPA app"
+    },
+    {
+        id: 3, 
+        title: "Practice useState()",
+        desc: "UseState() and useEffect()"
+     }
+];
+
+
+export default function TaskDetails() {
+    const { id } = useParams();
+
+    const task = tasks.find(task => task.id === Number(id));
+
+    if(!task) {
+        return <h1>Task not found</h1>;
+    }
+
+    return (
+        <div>
+            <h1>{task.title}</h1>
+            <p>{task.desc}</p>
+        </div>
+    );
+}
